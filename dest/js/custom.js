f2eb3ca7c3c26005043411625a25592ebdb43d32
Khang <khangdoan03121993@gmail.com>
2016-05-06 10:42:13 +0700

    add dom, effect slider, styles our rates section

--- dest/js/custom.js
+++ dest/js/custom.js
@@ -1,67 +1,93 @@
-(function(){
-	"use strict";
+(function() {
+    "use strict";
 
-	//slider
+    //slider
 
-	$(window).load(function() {
-    	$('.flexslider').flexslider({
-    		animation: "slide",
-    		slideshowSpeed: 3000,
-        controlNav: false,              
-        directionNav: false,  
-  		});
-  	});
+    $(window).load(function() {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            slideshowSpeed: 3000,
+            controlNav: false,
+            directionNav: false,
+        });
+    });
 
-  	//carousel
+    //carousel
 
-  	$(".owl-carousel").owlCarousel({
-	  	loop:true,
-	  	items:4,
-	  	autoplay:false,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true,
-		responsiveClass:true,
-	    responsive:{
-	        0:{
-	            items:1,
-	        },
-	        768:{
-	            items:2,
-	        },
-	        1024:{
-	            items:4,
-	        }
-	    }
-  	});
+    $(".owl-carousel1").owlCarousel({
+        loop: true,
+        items: 6,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            768: {
+                items: 4,
+            },
+            1024: {
+                items: 6,
+            }
+        }
+    });
 
-  	//set same height
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 4,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            1024: {
+                items: 4,
+            }
+        }
+    });
 
-  	    var highestBox = 0;
-        var highestBox1;
-        $('.services-in img').each(function(){  
-                if($(this).height() > highestBox){  
-                highestBox = $(this).height(); 
-                highestBox1 = highestBox;
-		        }
-		   	return highestBox1;
-    	}); 
+    //set same height
 
-    	$('.services-in img').height(highestBox1);
+    var highestBox = 0;
+    var highestBox1;
+    $('.services-in img').each(function() {
+        if ($(this).height() > highestBox) {
+            highestBox = $(this).height();
+            highestBox1 = highestBox;
+        }
+        return highestBox1;
+    });
+
+    $('.services-in img').height(highestBox1);
 
     //set same width
 
-    	var minwidth = 100;
-    	var minwidth1; 
+    var minwidth = 100;
+    var minwidth1;
 
-    	$('.services-in img').each(function(){  
-                if($(this).width() < minwidth){  
-                minwidth = $(this).width(); 
-                minwidth1 = minwidth;
-		        }
-		   	return minwidth1;
-    	}); 
+    $('.services-in img').each(function() {
+        if ($(this).width() < minwidth) {
+            minwidth = $(this).width();
+            minwidth1 = minwidth;
+        }
+        return minwidth1;
+    });
 
-    	$('.services-in img').width(minwidth1);
+    $('.services-in img').width(minwidth1);
+
+    //counter
+    $('.counter').counterUp({
+        delay: 10,
+        time: 4000
+    });
 
 })(jQuery);
 
